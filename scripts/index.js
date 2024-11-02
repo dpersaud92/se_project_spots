3ea14c4e215ac37228f36ca240bf1d668aad002e
Dwayne Persaud <dxp1020@gmail.com>
2024-11-02 17:17:52 -0400

    Opening and Closing the Modal

--- scripts/index.js
+++ scripts/index.js
@@ -26,3 +26,20 @@ const initialCards = [
 ];
 
 console.log(initialCards);
+
+const profileEditButton = document.querySelector(".profile__edit-btn");
+
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editModalCloseButton =
+  editProfileModal.querySelector(".modal__close-btn");
+
+function openModal() {
+  editProfileModal.classList.add("modal_opened");
+}
+
+function closeModal() {
+  editProfileModal.classList.remove("modal_opened");
+}
+
+profileEditButton.addEventListener("click", openModal);
+editModalCloseButton.addEventListener("click", closeModal);
