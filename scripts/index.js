0e96694c8f09c7b807c12ec6a575f596d404821e
Dwayne Persaud <dxp1020@gmail.com>
2024-11-09 17:31:50 -0500

    Update Project 4 - Final Stage

--- scripts/index.js
+++ scripts/index.js
@@ -28,12 +28,23 @@ const initialCards = [
 console.log(initialCards);
 
 const profileEditButton = document.querySelector(".profile__edit-btn");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const profileFormElement = document.querySelector(".modal__form");
+
+const nameInput = document.querySelector("#profile-name-input");
+const jobInput = document.querySelector("#profile-description-input");
 
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editModalCloseButton =
   editProfileModal.querySelector(".modal__close-btn");
 
+const cardTemplate = document.querySelector("#card-template").content;
+const cardsContainer = document.querySelector(".cards__list");
+
 function openModal() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileDescription.textContent;
   editProfileModal.classList.add("modal_opened");
 }
 
@@ -41,5 +52,33 @@ function closeModal() {
   editProfileModal.classList.remove("modal_opened");
 }
 
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  const newName = nameInput.value;
+  const newDescription = jobInput.value;
+  profileName.textContent = newName;
+  profileDescription.textContent = newDescription;
+  closeModal();
+}
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.cloneNode(true);
+
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitle.textContent = data.name;
+
+  return cardElement;
+}
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardsContainer.appendChild(cardElement);
+});
+
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
 profileEditButton.addEventListener("click", openModal);
 editModalCloseButton.addEventListener("click", closeModal);
